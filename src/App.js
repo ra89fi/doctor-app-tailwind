447949d2ce6c92251ea11ba49eae891838fac0b7
raihan <5421449+ra89fi@users.noreply.github.com>
2022-05-13 22:16:41 +0600

    navbar created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import About from './pages/About';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </div>
     );
