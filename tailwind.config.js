7247730a385eb48d2c222ad298f4cb5bb0901c83
raihan <5421449+ra89fi@users.noreply.github.com>
2022-05-13 21:49:52 +0600

    init pages

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [require('daisyui')],
+    daisyui: {
+        themes: [
+            {
+                doctortheme: {
+                    primary: '#0fcfec',
+                    secondary: '#19d3ae',
+                    accent: '#3a4256',
+                    neutral: '#3d4451',
+                    'base-100': '#ffffff',
+                },
+            },
+            'dark',
+            'cupcake',
+        ],
+    },
+};
